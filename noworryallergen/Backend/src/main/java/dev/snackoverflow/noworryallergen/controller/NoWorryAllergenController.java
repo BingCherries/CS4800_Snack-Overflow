fa17e74d73955c6a01670532a7f4ecd906838066
Noah (Hyeonwoo) Kim <84950847+noah-hw-kim@users.noreply.github.com>
2024-06-10 13:55:20 -0700

    tested SpringBootApp - working no issue

--- noworryallergen/Backend/src/main/java/dev/snackoverflow/noworryallergen/controller/NoWorryAllergenController.java
+++ noworryallergen/Backend/src/main/java/dev/snackoverflow/noworryallergen/controller/NoWorryAllergenController.java
@@ -1,7 +1,12 @@
 package dev.snackoverflow.noworryallergen.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class NoWorryAllergenController {
+    @GetMapping("/")
+    public String apiRoot() {
+        return "Hello, World";
+    }
 }
