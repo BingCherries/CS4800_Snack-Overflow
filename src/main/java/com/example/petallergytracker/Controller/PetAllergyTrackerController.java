61752ceb6b8abd300369ac375526ced91544568b
Noah (Hyeonwoo) Kim <84950847+noah-hw-kim@users.noreply.github.com>
2024-06-23 10:49:34 -0700

    1) added @crossorigin in controllers 2) added descriptions for HttpStatus in PetAllergyTrackerController for frontend

--- src/main/java/com/example/petallergytracker/Controller/PetAllergyTrackerController.java
+++ src/main/java/com/example/petallergytracker/Controller/PetAllergyTrackerController.java
@@ -15,6 +15,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin
 public class PetAllergyTrackerController {
 
     @Autowired
@@ -28,6 +29,9 @@ public class PetAllergyTrackerController {
     /*
     * ******** CRUD using foodRepository *********
     * */
+    //    HttpStatus.OK -> Created food successfully
+    //    HttpStatus.BAD_REQUEST -> Couldn't create food (necessary variables were missing to create Food object)
+    //    HttpStatus.INTERNAL_SERVER_ERROR -> Else errors
     @PostMapping("/food")
     public ResponseEntity<Food> createFood(@Valid @RequestBody Food food) {
         Food newFood = petAllergyTrackerService.createFood(food);
@@ -39,23 +43,27 @@ public class PetAllergyTrackerController {
         }
     }
 
-    @PostMapping("/foods")
-    public ResponseEntity<List<Food>> createFoods(@Valid @RequestBody List<Food> foods) {
-        List<Food> newFoods = petAllergyTrackerService.createFoods(foods);
+//    @PostMapping("/foods")
+//    public ResponseEntity<List<Food>> createFoods(@Valid @RequestBody List<Food> foods) {
+//        List<Food> newFoods = petAllergyTrackerService.createFoods(foods);
+//
+//        if (!newFoods.isEmpty()) {
+//            return new ResponseEntity<List<Food>>(newFoods, HttpStatus.OK);
+//        } else {
+//            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+//        }
+//    }
 
-        if (!newFoods.isEmpty()) {
-            return new ResponseEntity<List<Food>>(newFoods, HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-
+    //    HttpStatus.OK -> Got the food successfully
+    //    HttpStatus.NOT_FOUND -> The food was not in the DB
     @GetMapping("/food")
     public ResponseEntity<Food> getFood(@RequestParam ObjectId id) {
         Optional<Food> food = petAllergyTrackerService.getFood(id);
         return food.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
     }
 
+    //    HttpStatus.OK -> Got all foods in DB Successfully
+    //    HttpStatus.NOT_FOUND -> There are no foods in the DB
     @GetMapping("/foods")
     public ResponseEntity<List<Food>> getAllFoods() {
         List<Food> foods = petAllergyTrackerService.getAllFoods();
@@ -67,6 +75,8 @@ public class PetAllergyTrackerController {
         }
     }
 
+    //    HttpStatus.OK -> Got target foods in DB Successfully
+    //    HttpStatus.NOT_FOUND -> Target Foods were not in the DB
     @GetMapping("/foods/byIds")
     public ResponseEntity<List<Food>> getFoods(@RequestParam List<ObjectId> ids) {
         List<Food> foods = petAllergyTrackerService.getFoods(ids);
@@ -78,6 +88,8 @@ public class PetAllergyTrackerController {
         }
     }
 
+    //    HttpStatus.OK -> Deleted the food in DB Successfully
+    //    HttpStatus.NOT_FOUND -> The food was not in the DB
     @DeleteMapping("/food")
     public ResponseEntity<Food> deleteFood(@RequestParam ObjectId id) {
         Optional<Food> deletedFood = petAllergyTrackerService.deleteFood(id);
@@ -93,6 +105,9 @@ public class PetAllergyTrackerController {
     /*
      * ******** CRUD using AllergyReactionRepository *********
      * */
+    //    HttpStatus.OK -> Created allergic reaction successfully
+    //    HttpStatus.BAD_REQUEST -> Couldn't create allergic reaction (Necessary variables were missing to create AllergicReaction object)
+    //    HttpStatus.INTERNAL_SERVER_ERROR -> Else Error
     @PostMapping("/allergy")
     public ResponseEntity<AllergicReaction> createAllergicReaction(@Valid @RequestBody AllergicReaction allergicReaction) {
         AllergicReaction newAllergicReaction = petAllergyTrackerService.createAllergicReaction(allergicReaction);
@@ -115,13 +130,16 @@ public class PetAllergyTrackerController {
 //        }
 //    }
 
+    //    HttpStatus.OK -> Got the Allergic Reaction successfully
+    //    HttpStatus.NOT_FOUND -> The Allergic Reaction is not in DB
     @GetMapping("/allergy")
     public ResponseEntity<AllergicReaction> getAllergicReaction(@RequestParam ObjectId id) {
         Optional<AllergicReaction> allergicReaction = petAllergyTrackerService.getAllergicReaction(id);
         return allergicReaction.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
     }
 
-
+    //    HttpStatus.OK -> Got all Allergic Reactions successfully
+    //    HttpStatus.NOT_FOUND -> There were no Allergic Reactions in DB
     @GetMapping("/allergies")
     public ResponseEntity<List<AllergicReaction>> getAllAllergicReactions() {
         List<AllergicReaction> allergicReactions = petAllergyTrackerService.getAllAllergicReactions();
@@ -133,6 +151,8 @@ public class PetAllergyTrackerController {
         }
     }
 
+    //    HttpStatus.OK -> Got target Allergic Reactions successfully
+    //    HttpStatus.NOT_FOUND -> Target Allergic Reactions were not in DB
     @GetMapping("/allergies/byIds")
     public ResponseEntity<List<AllergicReaction>> getAllergicReactions(@RequestParam List<ObjectId> ids) {
         List<AllergicReaction> allergicReactions = petAllergyTrackerService.getAllergicReactions(ids);
@@ -144,6 +164,8 @@ public class PetAllergyTrackerController {
         }
     }
 
+    //    HttpStatus.OK -> Deleted the Allergic Reaction successfully
+    //    HttpStatus.NOT_FOUND -> The Allergic Reaction was not in DB
     @DeleteMapping("/allergy")
     public ResponseEntity<AllergicReaction> deleteAllergicReaction(@RequestParam ObjectId id) {
         Optional<AllergicReaction> deletedAllergyReaction = petAllergyTrackerService.deleteAllergicReaction(id);
@@ -155,7 +177,8 @@ public class PetAllergyTrackerController {
         petAllergyTrackerService.deleteAllAllergicReactions();
     }
 
-
+    //    HttpStatus.OK -> Returned the common allergens found
+    //    HttpStatus.NOT_FOUND -> Haven't found any common allergens
     @GetMapping("/commonallergens")
     public ResponseEntity<List<String>> getCommonAllergens() {
         List<String> commonAllergens = petAllergyTrackerService.getCommonAllergens();
@@ -167,6 +190,8 @@ public class PetAllergyTrackerController {
         }
     }
 
+    //    HttpStatus.OK -> Returned the common allergens in the given food
+    //    HttpStatus.NOT_FOUND -> Haven't found any common allergens in the given food
     @GetMapping("/allergyalert")
     public ResponseEntity<List<String>> getAllergyAlert(@RequestParam ObjectId id) {
         List<String> allergyAlert = petAllergyTrackerService.getAllergyAlert(id);
--- src/main/java/com/example/petallergytracker/Service/PetAllergyTrackerService.java
+++ src/main/java/com/example/petallergytracker/Service/PetAllergyTrackerService.java
@@ -6,7 +6,6 @@ import com.example.petallergytracker.Models.Food;
 import com.example.petallergytracker.Repository.AllergyListRepository;
 import com.example.petallergytracker.Repository.FoodRepository;
 import com.example.petallergytracker.Repository.AllergicReactionRepository;
-import jakarta.validation.constraints.Null;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
