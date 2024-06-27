b4d0140769126a6b282300f4f4bf364345feddde
Nishat Quyoum <nishatq9006@gmail.com>
2024-06-26 19:10:04 -0700

    added funtion to 'tracknow' btn

--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -1,8 +1,15 @@
 import BannerBackground from "../Images/paw-print.png";
 import BannerImage from "../Images/pet.png";
 import { FiArrowRight } from "react-icons/fi";
-
+import { useNavigate } from "react-router-dom";
 const Home = () => {
+
+  const navigate = useNavigate();
+
+  const handleTrackNowClick = () => {
+    navigate("/login");
+  };
+
   return (
     <div className="home-container">
       <div className="home-banner-container">
@@ -19,7 +26,7 @@ const Home = () => {
           Keep your pet's well-being front and center with our latest allergy tracking feature. Showcase your pet's health journey with our platform, ensuring you're always one-click away from managing their allergies effectively!
           </p>
 
-          <button className="secondary-button">
+          <button className="secondary-button" onClick={handleTrackNowClick}>
             Track Now <FiArrowRight />{" "}
           </button>
         </div>
